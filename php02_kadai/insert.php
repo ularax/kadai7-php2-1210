f85e4dd44e8843013c82e06ad38216809ceb8b2b
ularax <85717720+ularax@users.noreply.github.com>
2022-12-13 01:52:49 +0900

    Add files via upload

---
+++ php02_kadai/insert.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ * 1. index.phpのフォームの部分がおかしいので、ここを書き換えて、
+ * insert.phpにPOSTでデータが飛ぶようにしてください。
+ * 2. insert.phpで値を受け取ってください。
+ * 3. 受け取ったデータをバインド変数に与えてください。
+ * 4. index.phpフォームに書き込み、送信を行ってみて、実際にPhpMyAdminを確認してみてください！
+ */
+
+//1. POSTデータ取得
+$title = $_POST['title'];
+$url = $_POST['url'];
+$comment = $_POST['comment'];
+
+//2. DB接続します
+try {
+  //dbnameをいれる
+  //ID:'root', Password: xamppは 空白 ''
+  $pdo = new PDO('mysql:dbname=gs_bm;charset=utf8;host=localhost','root','');
+} catch (PDOException $e) {
+  exit('DBConnectError:'.$e->getMessage());
+}
+
+//３．データ登録SQL作成
+
+// 1. SQL文を用意
+$stmt = $pdo->prepare("INSERT INTO gs_bm_table(id, title, url, comment, date)
+                      VALUES(NULL, :title, :url, :comment, sysdate() )");
+
+//  2. バインド変数を用意
+// Integer 数値の場合 PDO::PARAM_INT
+// String文字列の場合 PDO::PARAM_STR
+
+$stmt->bindValue(':title', $title, PDO::PARAM_STR);
+$stmt->bindValue(':url', $url, PDO::PARAM_STR);
+$stmt->bindValue(':comment', $comment, PDO::PARAM_STR);
+
+//  3. 実行
+$status = $stmt->execute();
+
+//４．データ登録処理後
+if($status === false){
+  //SQL実行時にエラーがある場合（エラーオブジェクト取得して表示）
+  $error = $stmt->errorInfo();
+  exit('ErrorMessage:'.$error[2]);
+}else{
+  //５．index.phpへリダイレクト
+  header('Location: index.php');
+}
+?>
